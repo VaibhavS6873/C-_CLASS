cee7ed148badc98779615751d961b0674d5eeaab
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2026-01-21 17:37:25 +0530

    Add files via upload

---
+++ SESSION_52/MODULAR_DATE/Date.hpp
@@ -0,0 +1,21 @@
+#ifndef _DATE_HPP
+#define _DATE_HPP
+
+#include<iostream>
+class Date
+{
+    private:
+        int day, month, year;
+
+    public:
+        Date(int _day, int _month, int _year);
+        int getDay();
+        int getMonth();
+        int getYear();
+        void setDay(int newDay);
+        void setMonth(int newMonth);
+        void setYear(int newYear);
+        void show();
+};
+
+#endif //_DATE_HPP 
