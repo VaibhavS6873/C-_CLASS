2d50a142ca3c4ac7333a9949e49e6b36112302f7
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2026-01-21 16:43:00 +0530

    Add files via upload

---
+++ SESSION_25/STATIC_04/general_syntax_file.cpp
@@ -0,0 +1,8 @@
+class YourClassName
+{
+    private: 
+        static YourTypeName your_variable_name; // p1 
+}; 
+
+// p2 
+YourTypeName YourClassName::your_variable_name = your_initializer; 
---
+++ SESSION_25/STATIC_04/static_class_member.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+class Test
+{
+    private:
+        int x;
+        static int y; //part 1 of static data member of class
+};
+
+int Test::y = 100; //part 2 of static data member of class  
