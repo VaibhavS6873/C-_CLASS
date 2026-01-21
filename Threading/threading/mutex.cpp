0b958b6d49826ec75c9f2d72ac5792e662392563
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2026-01-21 22:56:11 +0530

    Add files via upload

---
+++ Threading/threading/mutex.cpp
@@ -0,0 +1,17 @@
+#include "thread.hpp"
+
+cpa::mutex::mutex()
+{
+    pthread_mutex_init(&mu, NULL);
+}
+
+void cpa::mutex::lock()
+{
+    pthread_mutex_lock(&mu);
+}
+
+void cpa::mutex::unlock()
+{
+    pthread_mutex_unlock(&mu);
+}
+
