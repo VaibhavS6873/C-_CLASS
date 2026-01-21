f31289d09248810c132cbb92cba6bf1cc3a1c110
Vaibhav Shewale <vaibhav.shewale20@gmail.com>
2026-01-21 21:46:11 +0530

    Add files via upload

---
+++ SESSION_54/eventualPicture.cpp
@@ -0,0 +1,28 @@
+int max(int a, int b)
+{
+    if(a < b)
+        return a;
+    return b;
+}
+
+float max(float a, float b)
+{
+    if(a < b)
+        return a;
+    return b;
+}
+
+//---------------------------------------
+int max_int(int a, int b)
+{
+    if(a < b)
+        return a;
+    return b;
+}
+
+float max_float(float a, float b)
+{
+    if(a < b)
+        return a;
+    return b;
+}
